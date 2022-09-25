b656630ffef83955340dfdf5676fb446363566ca
NiphoMathibela <94120861+NiphoMathibela@users.noreply.github.com>
2022-09-25 21:22:48 +0200

    Progress changes

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,49 +1,66 @@
-import { IonContent, IonPage, IonText, IonImg, IonIcon, IonItem, IonLabel } from "@ionic/react";
-import { helpOutline,notificationsOutline, createOutline } from 'ionicons/icons';
+import {
+  IonContent,
+  IonPage,
+  IonText,
+  IonImg,
+  IonIcon,
+  IonItem,
+  IonLabel,
+} from "@ionic/react";
+import {
+  helpOutline,
+  notificationsOutline,
+  createOutline,
+} from "ionicons/icons";
 import Toolbar from "../components/Toolbar";
 import "./Profile.css";
 
 function Profile(props) {
+  return (
+    <IonPage>
+      <Toolbar />
+      <IonContent fullscreen>
+        <IonText>
+          <h2 className="app-heading">Profile</h2>
+        </IonText>
 
-   
+        <div className="img-container">
+          <img src={props.img} />
+        </div>
 
-    return ( 
-        <IonPage>
-            <Toolbar/>
-            <IonContent fullscreen>
-                <IonText>
-                    <h2 className= "app-heading">Profile</h2>
-                </IonText>
+        <IonText>
+          <p>{props.username}</p>
+          <p>{props.email}</p>
+        </IonText>
 
-                <div className= "img-container">
-                    <img src= {props.img}/> 
-                </div>
+        <IonItem lines="none">
+          <IonLabel>
+            <p>
+              <IonIcon icon={createOutline} />
+              Edit
+            </p>
+          </IonLabel>
+        </IonItem>
 
-                    <IonText>
-                        <p>{props.username}</p>
-                        <p>{props.email}</p>
-                    </IonText>
+        <IonItem lines="none">
+          <IonLabel>
+            <p>
+              <IonIcon icon={notificationsOutline} /> Notifications{" "}
+            </p>
+          </IonLabel>
+        </IonItem>
 
-                <IonItem lines= "none">
-                    <IonLabel>
-                        <p><IonIcon icon= {createOutline}/>Edit</p>
-                    </IonLabel>
-                </IonItem>
-
-                <IonItem lines= "none">
-                    <IonLabel>
-                        <p><IonIcon icon= {notificationsOutline} /> Notifications </p>
-                    </IonLabel>
-                </IonItem>
-
-                <IonItem lines= "none">
-                    <IonLabel>
-                        <p><IonIcon icon= {helpOutline}/>Help</p>
-                    </IonLabel>
-                </IonItem>
-            </IonContent>
-        </IonPage>
-     );
+        <IonItem lines="none">
+          <IonLabel>
+            <p>
+              <IonIcon icon={helpOutline} />
+              Help
+            </p>
+          </IonLabel>
+        </IonItem>
+      </IonContent>
+    </IonPage>
+  );
 }
 
 export default Profile;
--- src/pages/Progress.jsx
+++ src/pages/Progress.jsx
@@ -1,40 +1,75 @@
-import { IonContent, IonPage, IonText } from "@ionic/react";
+import { IonContent, IonPage, IonText, IonButton } from "@ionic/react";
+import { useState } from "react";
 import Toolbar from "../components/Toolbar";
+import "./Progress.css";
 
 function Progress(props) {
-    return ( 
-        <IonPage>
-            <Toolbar/>
-            <IonContent fullscreen>
+   const [startingWeight, setStartingWeight] = useState(70);
+    const [percentage, setPercentage] = useState(0);
+  const [goalWeight, setGoalWeight] = useState(50);
+  const [currentWeight, setCurrentWeight] = useState(0);
 
-            <div className= "container">
-            <IonText>
-                    <h2>Progress</h2>
-                </IonText>
+  //Handling current weight input
+  const handleCurrentWeight = (e) => {
+    setCurrentWeight(e.target.value);
+  }
 
-                <IonText>
-                    <h3>You've wroked out for</h3>
-                    <p>{props.workHours}</p>
-                </IonText>
+//Calculating percentage progress of the goal
+  const progress = () => {
+    
+    const weightLost = startingWeight - currentWeight;
+    const difference = startingWeight - goalWeight;
+    
+    console.log(`Weight lost: ${weightLost}`)
+    setPercentage(weightLost/difference * 100);
+  }
 
-                <IonText>
-                    <h3>Burned calories</h3>
-                    <p>{props.burnedCalories}</p>
-                </IonText>
 
-                <IonText>
-                    <h3>Weight</h3>
-                    <p>{props.weight}</p>
-                </IonText>
+  return (
+    <IonPage>
+      <Toolbar />
+      <IonContent fullscreen>
+        <IonText>
+          <h2>Progress</h2>
+        </IonText>
 
-                <IonText>
-                    <h3>BMI</h3>
-                    <p>{props.bmi}</p>
-                </IonText>
+        <div className="stats-container">
+          <IonText>
+            <h3>You've wroked out for</h3>
+            <p>{props.workHours}</p>
+          </IonText>
+
+          <IonText>
+            <h3>Current weight</h3>
+            <input onChange= {handleCurrentWeight} value= {currentWeight}/>
+            <IonButton onClick= {progress}>Save changes</IonButton>
+          </IonText>
+
+          <IonText>
+            <h3>Burned calories</h3>
+            <p>{props.burnedCalories}</p>
+          </IonText>
+
+          <IonText>
+            <h3>Starting weight</h3>
+            <p>{props.weight}</p>
+          </IonText>
+
+          <IonText>
+            <h3>BMI</h3>
+            <p>{props.bmi}</p>
+          </IonText>
+
+          <div className="goal">Goal: {goalWeight}</div>
+          <div className="progress-container">
+            <div className="progress-bar">
+              <div className="bar" style= {{width: `${percentage}%`}}></div>
             </div>
-            </IonContent>
-        </IonPage>
-     );
+          </div>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
 }
 
 export default Progress;
