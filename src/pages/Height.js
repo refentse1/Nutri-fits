11cf0ce8d5510a775f9992bdf0ef964dcd725516
Nhlakanipho Mathibela <94120861+NiphoMathibela@users.noreply.github.com>
2022-10-21 17:48:05 +0200

    TargetWeight and target heught fuirebase inputs

--- src/pages/Height.js
+++ src/pages/Height.js
@@ -1,57 +1,63 @@
 import React from "react";
-import { 
-    IonBackButton, 
-    IonButton, 
-    IonButtons,    
-    IonContent, 
-    IonHeader,
-    IonInput,
-    IonItem,
-    IonLabel,
-    IonPage,
-    IonText,
-    IonTitle, 
-    IonToolbar
+import {
+  IonBackButton,
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonText,
+  IonTitle,
+  IonToolbar,
 } from "@ionic/react";
-import Logo from "../images/Logo.png"
-import "../components/Toolbar"
+import Logo from "../images/Logo.png";
+import "../components/Toolbar";
 import Toolbar from "../components/Toolbar";
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
 // import './style.css';
 
 const Height = (props) => {
-    const onChangeHeight = (event) => {
-        props.onAddHeight(event.target.value);
-    };
-    return (
-        <IonPage>
-            <Toolbar/>
-            <IonContent class="ion-padding">
-                <IonText className='profile' >My Profile</IonText>
-                <IonText>
-                    <p className='motivate'>"Baby steps always go<br/> the furthest!..."</p>
-                </IonText>
-                <IonText className='ion-weight'>
-                    What is <br/> your<br/> <IonText className='weight'>Height ?</IonText>
-                </IonText>
-                
-                <IonItem className='ion-item--weight'>
-                    <IonLabel position="floating">Height:</IonLabel>
-                    <IonInput
-                        type="number" 
-                        placeholder='(cm)' 
-                        // value={height} 
-                        onIonChange={onChangeHeight}
-                    />
-                </IonItem>
-                <p className='next-button'>
-                    <IonButton routerLink="/TargetWeight" shape='round' >
-                        <IonText style={{color:"#573926"}}>Next</IonText>
-                    </IonButton>
-                </p>
-            </IonContent>
+  const { heightInput, setHeightInput, addHeight } = useContext(AuthContext);
+  const onChangeHeight = (e) => {
+    setHeightInput(e.target.value);
+  };
+  return (
+    <IonPage>
+      <Toolbar />
+      <IonContent class="ion-padding">
+        <IonText className="profile">My Profile</IonText>
+        <IonText>
+          <p className="motivate">
+            "Baby steps always go
+            <br /> the furthest!..."
+          </p>
+        </IonText>
+        <IonText className="ion-weight">
+          What is <br /> your
+          <br /> <IonText className="weight">Height ?</IonText>
+        </IonText>
 
-        </IonPage>
-    );
-}
- 
+        <IonItem className="ion-item--weight">
+          <IonLabel position="floating">Height:</IonLabel>
+          <IonInput
+            type="number"
+            placeholder="(cm)"
+            value={heightInput}
+            onIonChange={onChangeHeight}
+          />
+        </IonItem>
+        <p className="next-button">
+          <IonButton routerLink="/TargetWeight" shape="round" onClick={addHeight}>
+            <IonText style={{ color: "#573926" }}>Next</IonText>
+          </IonButton>
+        </p>
+      </IonContent>
+    </IonPage>
+  );
+};
+
 export default Height;
--- src/pages/TargetWeight.js
+++ src/pages/TargetWeight.js
@@ -1,61 +1,66 @@
-import React from 'react';
-import { 
-    IonBackButton, 
-    IonButton, 
-    IonButtons,  
-    IonContent, 
-    IonHeader,
-    IonInput,
-    IonItem,
-    IonLabel,
-    IonPage,
-    IonText, 
-    IonTitle, 
-    IonToolbar
+import React from "react";
+import {
+  IonBackButton,
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonText,
+  IonTitle,
+  IonToolbar,
 } from "@ionic/react";
-import Logo from "../images/Logo.png"
-import "../components/Toolbar.css"
-import Toolbar from '../components/Toolbar';
-import {useContext} from "react"
-import {AuthContext} from "../contexts/AuthContext"
+import Logo from "../images/Logo.png";
+import "../components/Toolbar.css";
+import Toolbar from "../components/Toolbar";
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
 // import './style.css';
 
 const TargetWeight = (props) => {
-    const onChangeTargetWeight = (event) => {
-        props.onAddTargetWeight(event.target.value);
-    };
+    const { targetWeightInput, setTargetWeightInput, addGoal } = useContext(AuthContext);
+    
+  const onChangeTargetWeight = (e) => {
+   setTargetWeightInput(e.target.value);
+  };
 
-    return ( 
-        <IonPage>
-            <Toolbar/>
-            <IonContent class="ion-padding">
-                <IonText className='profile' >My Profile</IonText>
-                <IonText>
-                    <p className='motivate'>"Go the extra mile, <br/>no one will save you"</p>
-                </IonText>
-                <IonText className='ion-weight'>
-                    What is <br/> your goal<br/> 
-                    <IonText className='weight'>Weight ?</IonText>
-                </IonText>
+  return (
+    <IonPage>
+      <Toolbar />
+      <IonContent class="ion-padding">
+        <IonText className="profile">My Profile</IonText>
+        <IonText>
+          <p className="motivate">
+            "Go the extra mile, <br />
+            no one will save you"
+          </p>
+        </IonText>
+        <IonText className="ion-weight">
+          What is <br /> your goal
+          <br />
+          <IonText className="weight">Weight ?</IonText>
+        </IonText>
 
-                <IonItem className='ion-item--weight'>
-                    <IonLabel position="floating">Goal Weight:</IonLabel>
-                    <IonInput 
-                        type="number" 
-                        placeholder='(kg)' 
-                        // value={targetWeight} 
-                        onIonChange={onChangeTargetWeight}
-                    />
-                </IonItem>
-                <p className='next-button'>
-                    <IonButton routerLink="/setupscreen" shape='round' >
-                        <IonText style={{color:"#573926"}}>Next</IonText>
-                    </IonButton>
-                </p>
-            </IonContent>
-
-        </IonPage>
-    );
-}
+        <IonItem className="ion-item--weight">
+          <IonLabel position="floating">Goal Weight:</IonLabel>
+          <IonInput
+            type="number"
+            placeholder="(kg)"
+            value={targetWeightInput}
+            onIonChange={onChangeTargetWeight}
+          />
+        </IonItem>
+        <p className="next-button">
+          <IonButton routerLink="/setupscreen" shape="round" onClick={addGoal}>
+            <IonText style={{ color: "#573926" }}>Next</IonText>
+          </IonButton>
+        </p>
+      </IonContent>
+    </IonPage>
+  );
+};
 
 export default TargetWeight;
