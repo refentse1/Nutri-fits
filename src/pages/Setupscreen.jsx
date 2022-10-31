11feb5f605d562514edfd7c05705e1316238e146
refentse1 <refentsengoako101@gmail.com>
2022-10-31 18:17:03 +0200

    Upddating Katleho's changes

--- src/pages/Setupscreen.jsx
+++ src/pages/Setupscreen.jsx
@@ -1,65 +1,96 @@
 import {
-    IonButton,
-    IonCard,
-    IonCardTitle,
-    IonContent,
-    IonHeader,
-    IonInput,
-    IonItem,
-    IonLabel,
-    IonPage,
-    IonToolbar
-  } from "@ionic/react";
-  import "./Setupscreen.css"
-
-  const Setupscreen = (props) => {
-    // const { user, setUser } = useContext(UserContext);
-    // const history = useHistory();
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonImg,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonToolbar
+} from "@ionic/react";
+import { useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router";
+import "./Setupscreen.css"
+import { Camera, CameraResultType } from '@capacitor/camera';
+import {FaCamera} from "react-icons/fa"
+import {db,nicknameRef} from "../config/firebase-config"
+import{
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+  onSnapshot,
+} from "firebase/firestore"
+const Setupscreen = () => {
+  const history = useHistory();
+  const [Addnickname,setAddNickname]=useState([]);
+  const [newNickname,setNewNickname]=useState([]);
+  const[image,setImage]=useState("");
+  // useEffect(() => {
+  //   console.log("Form Data: ", formData);
+  // }, [formData]);
+  const Snap = async () => {
+    const cameraResult = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri
+    });
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    setImage (cameraResult.webPath);
   
-    // const [formData, setFormData] = useState({});
-  
-    // function handleSubmit() {
-    //   setUser([...user, formData]);
-    //   setFormData({});
-    //   history.push("/home", { direction: "forward" }); // go to home page
-    // }
-    // useEffect(() => {
-    //   console.log("Form Data: ", formData);
-    // }, [formData]);
-  
-    return (
-      <IonPage>
-              <IonContent className="profile-page">
-                <IonHeader>
-                  <IonToolbar>
-                    <IonCardTitle style={{ textAlign: "center" }} className="Sprofile">Fill your Profile</IonCardTitle>
-                  </IonToolbar>
-                </IonHeader>
-            <div className="img-container">
-                <img src={ "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"} />
-            </div>
-
-                <IonItem>
-                  <IonLabel position="floating" color="medium">Nickname</IonLabel>
-                  <IonInput
-                color="medium"
-                  className="Nickname"
-                    type="text"
-                    // onIonInput={(e) =>
-                    //   setFormData({ ...formData, nickname: e.target.value })
-                    // }
-                  />
-                </IonItem>
-                <IonButton className="next-bt"shape="round" size="small" >
-                  Next
-                </IonButton>
-                {/* <IonButton className="next-bt"shape="round" size="small" onClick={handleSubmit}>
-                  Next
-                </IonButton> */}
-              </IonContent>
-      </IonPage>
-    );
+    // Can be set to the src of an image now
+    // imageElement.src = imageUrl;
+    console.log(image)
   };
-  
-  export default Setupscreen;
-  
+  function handleSubmit() {
+    addDoc(nicknameRef,{
+      Nickname:newNickname,
+    }).then((refrence)=>{
+      console.log("Nickname moved")
+    }).catch((error)=>{
+      console.log(error)
+    })
+    setAddNickname("")
+    history.push("/home",{ direction: "forward" }); // go to home page
+  }
+  return (
+    <IonContent className="profile-page">
+  <div>
+      <img  className="img-container" src={image}></img>
+  </div>
+  < FaCamera className="iconn" onClick={()=>Snap()} 
+    color="#573926"
+    size="2em" 
+    title="Take picture or choos from your photos "/>
+      <IonItem>
+        <IonLabel position="floating" color="medium">Name:</IonLabel>
+        <IonInput
+        className="Name"
+          type="text"
+          color="medium"
+        />
+      </IonItem>
+      <IonItem>
+        <IonLabel position="floating" color="medium">Nickname:</IonLabel>
+        <IonInput
+      color="medium"
+         className="Nickname"
+          type="text"
+          onIonChange={(e)=>setNewNickname(e.target.value)}
+          value={newNickname}
+        />
+      </IonItem>
+      <IonButton className="next-bt"shape="round" size="small" onClick={handleSubmit}>
+        Next
+      </IonButton>
+    </IonContent>
+  );
+};
+
+export default Setupscreen;
